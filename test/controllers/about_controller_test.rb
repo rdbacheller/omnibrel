37a0691fc2b0281ac283d16b39f4cebedba8627c
rdbacheller <rd.bacheller@gmail.com>
2014-12-30 00:36:10 -0500

    Omnibrel

---
+++ test/controllers/about_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AboutControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get resources" do
+    get :resources
+    assert_response :success
+  end
+
+end
